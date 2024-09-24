3cdefd671eaa76577811c42fd3db625f2768eb14
ErikMilla <erikmilla456@gmail.com>
2024-09-24 17:19:44 +0000

    Catninos

--- script.js
+++ script.js
@@ -19,16 +19,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-const images = document.querySelectorAll('.card-img-top');
-
-images.forEach(image => {
-    const originalSrc = image.src; // Guarda la imagen original
-
-    image.addEventListener('mouseover', () => {
-        image.src = image.getAttribute('data-hover'); // Cambia a la imagen de hover
-    });
-
-    image.addEventListener('mouseout', () => {
-        image.src = originalSrc; // Restaura la imagen original
-    });
-});
